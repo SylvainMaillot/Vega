2047b71716ecdbb2e368996142fdacc99feec698
Sylvain <sylvain.maillot1@gmail.com>
2017-09-19 17:41:42 +0200

    ajout d'un fichier test

---
+++ test.py
@@ -0,0 +1,6 @@
+from sklearn_pandas 
+import CategoricalImputer
+data = np.array(['a', 'b', 'b', np.nan], dtype=object)
+imputer = CategoricalImputer()
+imputer.fit_transform(data)
+array(['a', 'b', 'b', 'b'], dtype=object)
