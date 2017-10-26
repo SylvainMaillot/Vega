1c223f037d36a31e35e35c553a425629354c1cfe
Serrah <sara387.thomas@hotmail.fr>
2017-10-26 10:13:03 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#Allo Allo
-
-from sklearn_pandas 
-import CategoricalImputer
-data = np.array(['a', 'b', 'b', np.nan], dtype=object)
-imputer = CategoricalImputer()
-imputer.fit_transform(data)
-array(['a', 'b', 'b', 'b'], dtype=object)
